b6f7d2c66730fc25e06d3c3b712b43b7d87968ad
Tommy Bouchard-Lebrun <tommy.bouchard-lebrun.1@ulaval.ca>
2025-07-11 10:43:42 -0400

    More robust and clean ROS packaging  by [git status]

--- mtt_ws/src/mtt_driver/setup.py
+++ mtt_ws/src/mtt_driver/setup.py
@@ -12,19 +12,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
-        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
-
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Alex',
     maintainer_email='alex.chorel-campanozzi@ecn.forces.gc.ca',
     description='ROS 2 driver for MTT-154 vehicle',
-    license='Apalache Liscense 2.0',
-    #tests_require=['pytest'],
+    license='Apache License 2.0',
     entry_points={
         'console_scripts': [
+            'mtt_driver = mtt_driver.mtt_driver:main',
+            'mtt_ros_wrapper = mtt_driver.mtt_ros_wrapper:main',
+            'mtt_teleop_joy = mtt_driver.mtt_teleop_joy:main',
+            'mtt_test_node = mtt_driver.mtt_test_node:main',
         ],
     },
 )
