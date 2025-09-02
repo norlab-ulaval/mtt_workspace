93c24acb8b16335b72e0916854dbe4dfddc4279b
admAssistant <steel4444@hotmail.com>
2025-09-02 13:56:03 -0400

    change from joy_node to joy_linux_node to bypass the issue where the joystick controller can't be read

--- mtt_ws/src/mtt_bringup/launch/mtt_teleop_controller.launch.py
+++ mtt_ws/src/mtt_bringup/launch/mtt_teleop_controller.launch.py
@@ -4,14 +4,23 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    # ros_joy_node = Node(
+    #     package='joy',
+    #     executable='joy_node',
+    #     name='joy_node',
+    #     output='screen',
+    #     parameters=[{'dev': '/dev/input/js0'}]
+    # )
+    
     ros_joy_node = Node(
-        package='joy',
-        executable='joy_node',
+        package='joy_linux',
+        executable='joy_linux_node',
         name='joy_node',
-        output='screen',
-        parameters=[{'dev': '/dev/input/js0'}]
+        parameters=[{
+            'deadzone': 0.15,
+            'device_name': '/dev/input/js0'
+        }]
     )
-
 
     
     mtt_joy_mapper = Node(
